cc0da3966e8eeb1b6ec003c1c855a9117c2bcd36
User Asukaru <asukaru@www2177.sakura.ne.jp>
2019-03-06 16:26:18 +0900

    Excel add

---
+++ sideBar.php
@@ -0,0 +1,12 @@
+<div id="sidebar">
+	<div class="list-group">
+		<a href="index.php" > TOP </a>
+		<a href="readfile.php" >完成文書を見る</a>
+		<a href="#" > テスト版 </a>
+		<a href="#"> テスト版 </a>
+		<a href="#" > テスト版 </a>
+		<a href="#" > テスト版 </a>
+		<a href="#" > テスト版 </a>
+		<a href="#" > テスト版 </a>
+</div>
+</div>
