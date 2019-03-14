d2eafc41db095bdff05b017c8003f8b674c8c4b0
unknown <asukaru2014@icloud.com>
2019-03-14 15:35:12 +0900

    修正

--- excelTest.php
+++ excelTest.php
@@ -23,50 +23,7 @@ $ary = $sheet->toArray(null,true,true,true);
 	$uploads_dir = './uploads/';
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-	<title>sample</title>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-	<style type="text/css">
-	#wrapp{
-		width: 77%;
-		padding: 12px;
-		margin-left: 5%;
-		float: left;
-	}
-	#sidebar{
-		float: left;
-		width: 18%;
-		background: #000080;
-		color: #fff;
-		height: 100vw;
-	}
-	#sidebar a:link {
-		color: #fff;
-		text-align: center;
-		margin: 15px;
-	}
-		
-		#files{
-			display: none;
-			width: 20%;
-			margin-left: 12px;
-			float: left;
-			z-index: 99;
-		}
-		.clear{
-			clear: both;
-		}
-	#fileForms{
-		margin-top: 12px;
-	}
-	</style>
-</head>
-<body>
+<?php include 'header.php'; ?>
 <?php include 'sideBar.php'; ?>
 <div id="wrapp">
 	<h2>アップロードされたファイルを読み込みました</h2>
